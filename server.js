52366af9072e7aa80174b95cacb06126772511b5
Sérgio Silva <a_k_a_bouncer@hotmail.com>
2022-01-06 01:22:08 +0000

    add: /api/whoami implementation

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.route("/api/whoami")
+  .get(function (req, res) {
+    res.json({
+      ipaddress: req.headers['x-forwarded-for'],
+      language: req.headers['accept-language'],
+      software: req.headers['user-agent'],
+    });
+  });
 
 
 // listen for requests :)
